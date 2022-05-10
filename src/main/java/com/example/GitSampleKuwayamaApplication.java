d80d05a9c91e561ad2d011c2ca4ab4c884ea27d8
saorikuwayama <saori.kuwayama@rakus-partners.co.jp>
2022-05-10 13:50:02 +0900

    first commit

---
+++ src/main/java/com/example/GitSampleKuwayamaApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleKuwayamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleKuwayamaApplication.class, args);
+	}
+
+}
